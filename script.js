51694938dde3534afa93c5261543fd85b1fc0c12
barbiedev <barbaracsliima@hotmail.com>
2022-08-09 14:24:39 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+function onScroll() {
+    showNavOnScroll()
+    showBackToTopButtonOnScroll()
+}
+function showNavOnScroll(){
+    if (scrollY > 0) {
+        navi.classList.add('scroll')
+    } else {
+        navi.classList.remove('scroll')
+    }
+}
+function showBackToTopButtonOnScroll(){
+    if (scrollY > 550) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
+function openMenu() {
+    document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+    origin: 'botton',
+    distace: '30px',
+    duration: 700,
+
+})
+    .reveal(`
+#home, 
+#home img, 
+#home .stats, 
+#services,
+#services header,
+#about,
+#about header,
+#about .content,
+#contact,
+#footer`)
