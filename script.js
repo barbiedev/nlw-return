036c1366942b23f0a210490cb4c8568581b0bcf5
Barbara Lima <87442887+barbiedev@users.noreply.github.com>
2022-08-19 16:21:52 -0300

    Final project

--- script.js
+++ script.js
@@ -4,15 +4,59 @@ onScroll()
 function onScroll() {
     showNavOnScroll()
     showBackToTopButtonOnScroll()
+
+    activateMenuAtCurrentSection(home)
+    activateMenuAtCurrentSection(services)
+    activateMenuAtCurrentSection(about)
+    activateMenuAtCurrentSection(contact)
+
+
+
 }
-function showNavOnScroll(){
+
+function activateMenuAtCurrentSection(section) {
+    //linha alvo
+    const targetLine = scrollY + innerHeight / 2
+
+    //verificar se a secao passou da linha
+    //quais dados vou precisar?
+    // o topo da secao
+    const sectionTop = section.offsetTop
+    //a altura da secao
+    const sectionHeight = section.offsetHeight
+
+    // o topo da secao chegou ou ultrapassoi a linha alvo
+    const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+
+    //verificar se a base esta abaixo dalinha alvo
+    //quais dados vou precisar?
+
+    // a secao termina onde?
+    const sectionEndsAt = sectionTop + sectionHeight
+    //final da secao passou da linha alvo?
+    const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+    //limites da secao
+    const sectionBoundaries = sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+    const sectionId = section.getAttribute('id')
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+    menuElement.classList.remove('active')
+    if (sectionBoundaries) {
+        menuElement.classList.add('active')
+    }
+
+}
+function showNavOnScroll() {
     if (scrollY > 0) {
         navi.classList.add('scroll')
     } else {
         navi.classList.remove('scroll')
     }
 }
-function showBackToTopButtonOnScroll(){
+function showBackToTopButtonOnScroll() {
     if (scrollY > 550) {
         backToTopButton.classList.add('show')
     } else {
